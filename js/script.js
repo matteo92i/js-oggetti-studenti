7f9795c29e5b52468b3cfcef5fddd0fcefa41f52
Matteo92i <matteo.iovane@gmail.com>
2021-09-08 09:55:56 +0200

    piccole corr

--- js/script.js
+++ js/script.js
@@ -30,35 +30,16 @@ for (var i = 0; i < listaStudenti.length; i++) {
     console.log(listaStudenti[i].nome, listaStudenti[i].cognome);
    }
 
-var classe = {
-    name: 'iovane',
-    students: [
-        {
-            nome: 'matteo',
-            cognome: 'iovane',
-            age: 29
-        },
-        {
-            nome: 'Gianluca',
-            cognome: 'Iovane',
-            age: 25
-        }
-    ]
-}
-
-for (var x = 0; x < classe.students.length; x++) {
-    console.log(classe.students[x].nome, classe.students[x].cognome);
-    
-}
+   var nuovoNome = prompt('Inserisci il nome dello studente');
+   
+   var nuovoCognome = prompt('Inserisci il cognome dello studente');
+   var nuovaEtà = prompt('Inserisci l\'età dello studente');
+   
+   listaStudenti.push({
+       nome: nuovoNome,
+       cognome: nuovoCognome,
+       age: nuovaEtà
+   });
 
 
-
-var nuovoNome = prompt('Inserisci il nome dello studente');
-var nuovoCognome = prompt('Inserisci il cognome dello studente');
-var nuovaEtà = prompt('Inserisci l\'età dello studente');
-
-studentsList.push({
-    nome: nuovoNome,
-    cognome: nuovoCognome,
-    age: nuovaEtà
-});
+console.log(listaStudenti)
